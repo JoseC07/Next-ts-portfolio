8caa17d2287867525eed576030c7e9a311f635b7
Jose Caudillo <caudillojose5@gmail.com>
2024-01-19 15:21:04 -0800

    feat: tailwind next.js component for porfilecard(Intro)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Intro from '@/components/Intro/Intro';
+
 import Image from 'next/image'
 
 export default function Home() {
   return (
+    <div className='bg-cover bg-center w-full h-screen flex justify-center items-center bg-black'>
+      <Intro></Intro>
+    </div>
     
-   
-    <Intro></Intro>
- 
 );
 };
   
---
+++ components/Contact/contactButton.tsx
@@ -0,0 +1,73 @@
+'use-client';
+import { CopyIcon } from "@radix-ui/react-icons"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { InferGetStaticPropsType } from "next";
+
+export default function contactButton(){
+
+    return (
+
+        <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Get in Touch Directly</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Personal Email</DialogTitle>
+          <DialogDescription>
+            copy José's email handle
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            {/* <Form></Form> */}
+            
+            
+            
+            <Input
+              id="link"
+              defaultValue="caudillojose5@gmail.com"
+              readOnly
+            />
+          </div>
+          <Button type="submit" size="sm" className="px-3">
+            <span className="sr-only">Copy</span>
+            <CopyIcon className="h-4 w-4" />
+          </Button>
+        
+        
+        
+        
+        
+        
+        
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+
+   
+    );
+}
